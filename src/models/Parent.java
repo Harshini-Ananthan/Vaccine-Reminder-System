94b3dbc14b57a418f1639c844e43dd2c796f2162
Harshini-Ananthan <harshiniananthan@protonmail.com>
2025-09-19 21:13:15 +0530

    Add Parent.java

--- src/models/Parent.java
+++ src/models/Parent.java
@@ -1,5 +1,24 @@
 package models;
 
-public class Parent {
-    
+public class Parent extends User {
+    private Child child;
+
+    public Parent(String name, String contact, Child child) {
+        super(name, contact);
+        this.child = child;
+    }
+
+    public Child getChild() {
+        return child;
+    }
+
+    @Override
+    public void generateReport() {
+        System.out.println("--- Parent Report ---");
+        System.out.println("Parent Name: " + name);
+        System.out.println("Contact: " + contact);
+        System.out.println("Child Name: " + child.getName());
+        System.out.println("Vaccine: " + child.getVaccineName());
+        System.out.println("Next Dose Date: " + child.getNextDoseDate());
+    }
 }
