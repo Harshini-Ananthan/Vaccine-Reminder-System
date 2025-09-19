3cc310934c970d8e809abc3d617a4f120778f731
Harshini-Ananthan <harshiniananthan@protonmail.com>
2025-09-19 21:02:32 +0530

    Add User.java

--- src/models/User.java
+++ src/models/User.java
@@ -1,5 +1,22 @@
 package models;
 
-public class User {
-    
+public abstract class User {
+    protected String name;
+    protected String contact;
+
+    public User(String name, String contact) {
+        this.name = name;
+        this.contact = contact;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    // Polymorphic method
+    public abstract void generateReport();
 }
